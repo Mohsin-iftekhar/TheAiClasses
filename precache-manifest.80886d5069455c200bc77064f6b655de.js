6c7c2eab50659acb2c509b1a71da863cedcdffa3
Mohsin-iftekhar <85876751+Mohsin-iftekhar@users.noreply.github.com>
2022-11-01 00:00:37 +0530

    Updates

---
+++ precache-manifest.80886d5069455c200bc77064f6b655de.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf61d19a75ceddf84f3d0c0c250ee726",
+    "url": "/TheAiClasses/index.html"
+  },
+  {
+    "revision": "dd389c2da81774a4f350",
+    "url": "/TheAiClasses/static/css/2.17f5e74d.chunk.css"
+  },
+  {
+    "revision": "b5077dc49c1ca549013a",
+    "url": "/TheAiClasses/static/css/main.f86cedf1.chunk.css"
+  },
+  {
+    "revision": "dd389c2da81774a4f350",
+    "url": "/TheAiClasses/static/js/2.edc43b90.chunk.js"
+  },
+  {
+    "revision": "b5077dc49c1ca549013a",
+    "url": "/TheAiClasses/static/js/main.8e702193.chunk.js"
+  },
+  {
+    "revision": "81c4def64c2bb1ae9136",
+    "url": "/TheAiClasses/static/js/runtime~main.198d8361.js"
+  },
+  {
+    "revision": "87371574994deeedd3d1c037b3c82d98",
+    "url": "/TheAiClasses/static/media/imbeside.87371574.png"
+  },
+  {
+    "revision": "b96c012aa07e4aaba0eaf3d8cddb54a1",
+    "url": "/TheAiClasses/static/media/mental.b96c012a.svg"
+  },
+  {
+    "revision": "c7dc1726a6718b386d2ba243a552a9d8",
+    "url": "/TheAiClasses/static/media/smallcam.c7dc1726.svg"
+  },
+  {
+    "revision": "a1a90a68ddd0568adbd67e17ee1f6ea8",
+    "url": "/TheAiClasses/static/media/snap.a1a90a68.svg"
+  },
+  {
+    "revision": "6c3064345ef233cc41eb9f195f5668ba",
+    "url": "/TheAiClasses/static/media/student.6c306434.svg"
+  }
+]);
